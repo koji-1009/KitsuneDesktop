84b7fe9afd2da20d27ade5489b5fa94b146f7a66
Koji Wakamiya <koji.wakamiya@gmail.com>
2017-01-25 23:15:25 +0900

    MITライセンスを適用

--- src/main/java/com/sf_lolitahag/Main.java
+++ src/main/java/com/sf_lolitahag/Main.java
@@ -1,3 +1,9 @@
+/*
+Copyright (C) 2016 Koji Wakamiya
+Copyright (C) 2016 Masanori Kojima
+Released under the MIT license
+http://opensource.org/licenses/mit-license.php
+*/
 package com.sf_lolitahag;
 
 import com.sf_lolitahag.panel.AbstractPanel;
